d071a295207900667e774f10ca120bcd23ee76b1
maq7 <maxpet16@mail.ru>
2021-01-21 16:03:55 +0500

    Ready logic of game-quiz.

--- src/API.ts
+++ src/API.ts
@@ -8,7 +8,7 @@ export type Question = {
     type: string,
 
 }
-export type QuestionState = Question & { answers: string }
+export type QuestionState = Question & { answers: string[] }
 export enum Difficulty {
     EASY = 'easy',
     MEDIUM = 'medium',
